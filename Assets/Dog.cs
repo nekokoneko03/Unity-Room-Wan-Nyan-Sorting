f8be20de7379160d78baa9c37fabbc5a81125aef
okaerinasai39 <ryousuke1039@gmail.com>
2022-12-05 23:54:26 +0900

    Add prefabs
    Add prefabs and animal scripts

---
+++ Assets/Dog.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.DOMove(new Vector3(5f, 0f, 0f), 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
